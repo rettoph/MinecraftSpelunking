55842d26bd6770da4403e677ad52bf4e4c28dc0a
rettoph <anthony@turner.ec>
2024-02-01 00:41:08 -0700

    Add LettuceEncrypt configuration

--- src/4 - presentation/MinecraftSpelunking.Presentation.WebServer/Program.cs
+++ src/4 - presentation/MinecraftSpelunking.Presentation.WebServer/Program.cs
@@ -1,4 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Configuration["LettuceEncrypt:Enabled"] == "true")
+{
+    builder.Services.AddLettuceEncrypt();
+}
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!!!!");
