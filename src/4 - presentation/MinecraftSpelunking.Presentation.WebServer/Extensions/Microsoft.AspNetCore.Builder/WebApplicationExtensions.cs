91fa9021a901b57fa619c1c2f077b9d77b6f7963
rettoph <anthony@turner.ec>
2024-02-09 11:50:58 -0700

    Add migration retries on startup

--- src/4 - presentation/MinecraftSpelunking.Presentation.WebServer/Extensions/Microsoft.AspNetCore.Builder/WebApplicationExtensions.cs
+++ src/4 - presentation/MinecraftSpelunking.Presentation.WebServer/Extensions/Microsoft.AspNetCore.Builder/WebApplicationExtensions.cs
@@ -3,22 +3,44 @@ using MinecraftSpelunking.Domain.Database;
 
 namespace MinecraftSpelunking.Presentation.WebServer.Extensions.Microsoft.AspNetCore.Builder
 {
-    public static class WebApplicationExtensions
-    {
-        public static async Task ApplyMigrationsAndRunAsync(this WebApplication app)
-        {
-            using (var scope = app.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
+	public static class WebApplicationExtensions
+	{
+		public static async Task ApplyMigrationsAndRunAsync(this WebApplication app)
+		{
+			using (var scope = app.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
 
-                var context = services.GetRequiredService<DataContext>();
-                if ((await context.Database.GetPendingMigrationsAsync()).Any())
-                {
-                    context.Database.Migrate();
-                }
-            }
+				var context = services.GetRequiredService<DataContext>();
 
-            await app.RunAsync();
-        }
-    }
+				var currentAttempt = 0;
+				var maxAttempts = 3;
+				while (currentAttempt < maxAttempts)
+				{
+					try
+					{
+						if ((await context.Database.GetPendingMigrationsAsync()).Any())
+						{
+							context.Database.Migrate();
+						}
+
+						break;
+					}
+					catch (Exception ex)
+					{
+						var logger = services.GetRequiredService<ILogger<WebApplication>>();
+						logger.LogError(ex, "There was an error applying migrations. Attempt {CurrentAttempt}/{MaxAttempts}", currentAttempt++, maxAttempts);
+
+						if (currentAttempt == maxAttempts)
+						{
+							logger.LogError("Too many migration attempts failed. Goodbye.");
+							return;
+						}
+					}
+				}
+			}
+
+			await app.RunAsync();
+		}
+	}
 }
